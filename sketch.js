15276e35b7aef379fede52ec4f2b8c34d366bd1a
Luiz-Gustavo-MV <164198468+Luiz-Gustavo-MV@users.noreply.github.com>
2024-04-17 10:59:19 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 var notas = [40,30,80,75,69,71,70,56,36]
 
-function stup() {
-  createCavas(400, 400);
+function setup() {
+  createCanvas(400, 400);
   for(var i=0; i<notas.length;i++) {
     if (notas[i] >= 70) {
       console.log("Aprovado " + notas[i])
